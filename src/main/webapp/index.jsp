70f17640cf896187dab68ab55c8611ac89ef005e
Supriya Mutharasan <supriya.mutharasan@ontariotechu.net>
2023-04-02 22:53:04 -0400

    created the version One template folder, code is hella messy

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Zocial Web Application</title>
+    <title>JSP - Hello World</title>
 </head>
 <body>
 <h1><%= "Hello World!" %>
 </h1>
 <br/>
-<a href="zocial-servlet">Zocial Servlet</a>
+<a href="hello-servlet">Hello Servlet</a>
 </body>
 </html>
--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,221 +1,123 @@
 let ws;
-let UserName = "";
-let CurrentRoom = "";
-let ListOfRooms = [];
-let IndexPageAreaToUpdateRef = document.getElementById("area-to-update");
-let ListOfRoomsTemplateRef = document.getElementById("ListOfRoomsTemplate");
-let MessageInputAreaFormRef = document.getElementById(
-    "messager-inputarea-form"
-);
 
-let ListOfMessagesAreaRef = document.getElementById("messager-chat");
 
-function RightBubbleMessageHTML(
-    givenUsername,
-    givenMessage,
-    capturedTimeStamp
-) {
-    return `<div class="message right-message">
-              <div
-                class="message-img"
-                style="
-                  background-image: url(https://avatars.githubusercontent.com/u/90413603?v=4);
-                "
-              ></div>
-
-              <div class="message-bubble">
-                <div class="message-info">
-                  <div class="message-info-name">${givenUsername}</div>
-                  <div class="message-info-time">${capturedTimeStamp}</div>
-                </div>
-
-                <div class="message-text">${givenMessage}</div>
-              </div>
-            </div>`;
-}
-
-function LeftBubbleMessageHTML(givenUsername, givenMessage, capturedTimeStamp) {
-    return `<div class="message left-message">
-              <div
-                class="message-img"
-                style="
-                  background-image: url(https://avatars.githubusercontent.com/u/59520945?v=4);
-                "
-              ></div>
-
-              <div class="message-bubble">
-                <div class="message-info">
-                  <div class="message-info-name">${givenUsername}</div>
-                  <div class="message-info-time">${capturedTimeStamp}</div>
-                </div>
-
-                <div class="message-text">${givenMessage}</div>
-              </div>
-            </div>`;
-}
-
-let CreateAndJoinChatRoomBtnRef = document.getElementById(
-    "CreateAndJoinChatRoomBtn"
-);
-
-function indexMainFunc() {
-    //newRoom();
-    CreateAndJoinChatRoomBtnRef.style.visibility = "hidden";
-    GetListOfRooms();
-}
-
-function setUserName() {
-    //access myForm using document object
-    UserName = document.getElementById("usernameInput").value;
-    IndexPageAreaToUpdateRef.innerHTML = `
-  <div class="jumbotron">
-    <h1 class="display-4">Welcome, ${UserName}</h1>
-    <p class="lead"> Please use the side bar to join any room you wish to chat in. </p>
-    <hr class="my-4">
-    <p>Alternatively, use the 'Create button' to create your own chat room!</p>
-    <p class="lead">
-        <a class="btn btn-primary btn-lg" href="about.html" role="button">About us</a>
-    </p>
-    </div>
-    `;
-    CreateAndJoinChatRoomBtnRef.style.visibility = "visible";
-}
-
-MessageInputAreaFormRef.addEventListener("submit", ProcessMessageInputted);
-
-function ProcessMessageInputted(event) {
-    let messageInputtedVal = document.getElementById("messager-input").value;
-    console.log(" messageInputtedVal " + messageInputtedVal);
-
-    let request = {
-        type: "chat",
-        chatRoomCode: GetCurrentRoom(),
-        msg: messageInputtedVal,
-    };
-    ws.send(JSON.stringify(request));
-    document.getElementById("messager-input").value = "";
-    event.preventDefault();
-}
-
-function GetListOfRooms() {
-    ListOfRoomsTemplateRef.innerHTML = ``;
-    // calling the ChatServlet to retrieve a new room ID
-    let callURL =
-        "http://localhost:8080/Zocial-1.0-SNAPSHOT/zocial-servlet/rooms";
-    fetch(callURL, {
-        method: "GET",
-        headers: {
-            Accept: "text/plain",
-        },
-    })
-        .then((response) => response.text())
-        .then((response) => {
-            console.log(response);
-            console.log(JSON.parse(response));
-            ListOfRooms = JSON.parse(response);
-            if (ListOfRooms.length != 0) {
-                for (room in ListOfRooms) {
-                    const tempLI = document.createElement("li");
-                    tempLI.innerHTML = `<a class="nav-item nav-link" href="">${ListOfRooms[room]}</a>`;
-                    ListOfRoomsTemplateRef.insertAdjacentElement("beforeend", tempLI);
+var network = function (websocket) {
+    return {
+        initialize: function() {
+            var url = 'ws://localhost:8080/WSChatServerDemo-1.0-SNAPSHOT/ws/hello';
+            websocket = new WebSocket(url);
+            // websocket.name = APP.id;
+            websocket.onopen = function(evt) {
+                //alert('onopen');
+            };
+            websocket.onerror = function(evt) {
+                alert('onerror');
+            };
+            websocket.onmessage = function (evt) {
+                //alert('onmessage');
+                var command = JSON.parse(evt.data);
+                console.log(command)
+                if (command.type == "pause") {
+                    APP.pauseVideo();
+                } else if (command.type == "play") {
+                    APP.playVideo();
+                } else if (command.type == "seeked") {
+                    APP.seekVideo(command.currentTime);
+                } else {
+                    alert("Unknown command " + command);
                 }
-            }
-        });
-}
-function newRoom() {
-    // calling the ChatServlet to retrieve a new room ID
-    let callURL = "http://localhost:8080/Zocial-1.0-SNAPSHOT/zocial-servlet";
-    fetch(callURL, {
-        method: "GET",
-        headers: {
-            Accept: "text/plain",
+            };
+            websocket.onclose = function()
+            {
+                alert('onclose');
+            };
         },
-    })
-        .then((response) => response.text())
-        .then((response) => {
-            var ChatRoomTitleRef = document.getElementById("chat-room-header-title");
-            ChatRoomTitleRef.innerHTML = `${response}`;
-            SetCurrentRoom(response);
-            return response;
-        })
-        .then((response) => {
-            GetListOfRooms();
-            enterRoom(response);
-        }); // enter the room with the code
+        send: function(command) {
+            console.log(" GOING To send the command " + command + " to server")
+            websocket.send(command);
+        }
+    }
+};
 
-    IndexPageAreaToUpdateRef.remove();
-    CreateAndJoinChatRoomBtnRef.style.visibility = "hidden";
-}
+var APP = {
+    id: Math.floor(Math.random() * 10000),
 
-function SetCurrentRoom(givenCode) {
-    CurrentRoom = givenCode;
-}
-function GetCurrentRoom() {
-    return CurrentRoom;
-}
+    network: network(null),
 
-function enterRoom(code) {
-    // refresh the list of rooms
+    // Cannot use 'this' here after updating window.onload (see below)
+    initialize: function () {
+        APP.network.initialize();
+        var video = APP.getVideo();
+        video.addEventListener('play',
+            function (event) {
+                console.log(" Video is supposed to be played")
+                var command = { type: "play" };
+                APP.network.send(JSON.stringify(command));
+            },
+            false);
+        video.addEventListener('pause',
+            function (event) {
+                console.log(" Video is supposed to be paused")
+                var command = { type: "pause" };
+                APP.network.send(JSON.stringify(command));
+            },
+            false);
+        video.addEventListener('seeked',
+            function (event) {
+                console.log(" Video is supposed to be seeked")
 
-    // create the web socket
-    ws = new WebSocket(
-        "ws://localhost:8080/Zocial-1.0-SNAPSHOT/ws/" + code
-    );
+                var command = { type: "seeked",
+                    currentTime: APP.getVideo().currentTime };
+                APP.network.send(JSON.stringify(command));
+            },
+            false);
+    },
 
-    if (UserName == "") {
-        console.error(
-            "Something went wrong. Cannot send username to the server. Please restart application"
-        );
-        throw new UserException(
-            "Something went wrong. Cannot send username to the server. Please restart application"
-        );
+    getVideo: function () {
+        console.log(document.getElementsByTagName("video")[0])
+        return document.getElementsByTagName("video")[0];
+    },
+
+    pauseVideo: function () {
+        var video = this.getVideo();
+        video.pause();
+    },
+
+    playVideo: function () {
+        var video = this.getVideo();
+        video.play();
+    },
+
+    seekVideo: function (currentTime) {
+        var video = this.getVideo();
+        video.currentTime = currentTime;
     }
 
-    let request = {
-        type: "setUserName",
-        msg: UserName,
-    };
+};
 
-    console.log(JSON.stringify(request));
+function enterRoom() {
+    let code = document.getElementById("room-code").value;
+    ws = new WebSocket("ws://localhost:8080/WSChatServerDemo-1.0-SNAPSHOT/ws/" + code);
 
-    ws.onopen = () => ws.send(JSON.stringify(request));
-
-    // parse messages received from the server and update the UI accordingly
     ws.onmessage = function (event) {
         console.log(event.data);
-        // parsing the server's message as json
         let message = JSON.parse(event.data);
-        console.log(" Message " + message.message);
-        console.log(" Username " + message.username);
-        const wrapperDiv = document.createElement("div");
-        wrapperDiv.style.marginBottom = "10px";
-        // handle message
-        var capturedTimeStamp = new Date().toLocaleTimeString();
-        if (message.username == UserName) {
-            wrapperDiv.innerHTML = RightBubbleMessageHTML(
-                UserName,
-                message.message,
-                capturedTimeStamp
-            );
-            ListOfMessagesAreaRef.insertAdjacentElement("beforeend", wrapperDiv);
-        } else if (message.username == "Server") {
-            wrapperDiv.classList.add("form-tex");
-            wrapperDiv.classList.add("text-muted");
-            wrapperDiv.style.marginBlock = "1.25rem";
-            wrapperDiv.style.textAlign = "center";
-            wrapperDiv.innerHTML = message.message;
-            ListOfMessagesAreaRef.insertAdjacentElement("beforeend", wrapperDiv);
-        } else {
-            wrapperDiv.innerHTML = LeftBubbleMessageHTML(
-                message.username,
-                message.message,
-                capturedTimeStamp
-            );
-            ListOfMessagesAreaRef.insertAdjacentElement("beforeend", wrapperDiv);
-        }
-    };
+        document.getElementById("log").value += "[" + timestamp() + "] " + message.message + "\n";
+    }
 }
+document.getElementById("input").addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        let request = {"type":"chat", "msg":event.target.value};
+        ws.send(JSON.stringify(request));
+        event.target.value = "";
+    }
+});
 
-(function () {
-    indexMainFunc();
-})();
+window.onload = APP.initialize;
+
+
+function timestamp() {
+    var d = new Date(), minutes = d.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    return d.getHours() + ':' + minutes;
+}
