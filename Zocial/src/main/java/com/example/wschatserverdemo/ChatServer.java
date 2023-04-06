db8bd901eef1b9243e0372546a7bf1fe56e39279
Jake Sullivan <90413603+snakesully@users.noreply.github.com>
2023-04-05 23:00:55 -0400

    Implemented Chat Histories
    Added folder util, ResourceAPI.java file

--- Zocial/src/main/java/com/example/wschatserverdemo/ChatServer.java
+++ Zocial/src/main/java/com/example/wschatserverdemo/ChatServer.java
@@ -9,31 +9,69 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.example.util.ResourceAPI.loadChatRoomHistory;
+import static com.example.util.ResourceAPI.saveChatRoomHistory;
+
+
 @ServerEndpoint(value="/ws/{roomID}")
 public class ChatServer {
 
     private Map<String, String> usernames = new HashMap<String, String>();
     private static Map<String, String> roomList = new HashMap<String, String>();
+
+    private static Map<String, String> roomHistoryList = new HashMap<String, String>();
     @OnOpen
     public void open(@PathParam("roomID") String roomID, Session session) throws IOException, EncodeException {
         //session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server ): Welcome to the chat room. Please state your username to begin.\"}");
+
+        roomList.put(session.getId(), roomID); // adding userID to a room
+
+        String history = loadChatRoomHistory(roomID);
         System.out.println("Room number is " + roomID);
+
+        if (history!=null && !(history.isBlank())){
+            System.out.println(history);
+            history = history.replaceAll(System.lineSeparator(), "\\\n");
+            System.out.println(history);
+            session.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\""+history+" \\n Chat room history loaded\"}");
+            roomHistoryList.put(roomID, history+" \\n "+roomID + " room resumed.");
+        }
+        if(!roomHistoryList.containsKey(roomID)) { // only if this room has no history yet
+            roomHistoryList.put(roomID, roomID + " room Created."); //initiating the room history
+        }
+
         System.out.println(" New client entered " + session.getId());
+
+
         //roomList.put(session.getId(), roomID); // adding userID to a room
     }
 
     @OnClose
     public void close(Session session) throws IOException, EncodeException {
-        System.out.println("Called for no reason");
         String userId = session.getId();
         if (usernames.containsKey(userId)) {
             String username = usernames.get(userId);
             String roomID = roomList.get(userId);
             usernames.remove(userId);
+            // remove this user from the roomList
+            roomList.remove(roomID);
+
+            // adding event to the history of the room
+            String logHistory = roomHistoryList.get(roomID);
+            roomHistoryList.put(roomID, logHistory+" \\n " + username + " left the chat room.");
+
+            // broadcasting it to peers in the same room
+            int countPeers = 0;
             for (Session peer : session.getOpenSessions()){ //broadcast this person left the server
                 if(roomList.get(peer.getId()).equals(roomID)) { // broadcast only to those in the same room
                     peer.getBasicRemote().sendText("{\"type\": \"chat\", \"message\":\"(Server): " + username + " left the chat room.\"}");
+                    countPeers++; // count how many peers are left in the room
                 }
+            }
+
+            // if everyone in the room left, save history
+            if(!(countPeers >0)){
+                saveChatRoomHistory(roomID, roomHistoryList.get(roomID));
             }
         }
     }
